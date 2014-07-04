4492871cee2e6913bf909a9a2a413d98cfa5f415
vkharseev <vladimir@realitygroup.ru>
2014-07-04 11:57:26 +0400

    Deploy через GIT

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,7 +2,8 @@
 
 $route['default_controller'] = "users_interface";
 $route['404_override'] = '';
-
+/******************************************************** GIT INTRERFACE **************************************************/
+$route['git-deploy/:any'] = "git_interface/gitDeployProject";
 /************************************************** CLIENT INTRERFACE ***********************************************/
 
 $route['cabinet/profile']			= "clients_interface/profile";
