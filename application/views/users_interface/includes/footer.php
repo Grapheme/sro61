b57ee729bdc718ec9267721c354a422a8f757a6c
vkharseev <vladimir@realitygroup.ru>
2014-07-04 12:38:06 +0400

    test

--- application/views/users_interface/includes/footer.php
+++ application/views/users_interface/includes/footer.php
@@ -1,4 +1,5 @@
 <div id="footer">
 	<div class="copy_1">© 2012 — Саморегулируемая организация некоммерческое партнерство «Энергоаудит»</div>
 	<div class="copy_2">Поддержка и продвижение сайта <br/> &mdash; Веб-мастерская «<a href="http://grapheme.ru">Графема</a>»<br><br><br></div>
+    <fack you></fack>
 </div>
