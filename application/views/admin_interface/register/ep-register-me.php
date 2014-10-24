cd71a96c1b625a626e42e0d4f83f538a549d065b
Владимир <vkharseev@gmail.com>
2014-10-24 19:07:09 +0400

    Новые документы

--- application/views/admin_interface/register/ep-register-me.php
+++ application/views/admin_interface/register/ep-register-me.php
@@ -21,7 +21,11 @@
 					энергопаспорт является повторным, для дальнейшего направления в Минэнерго.
 				</p>-->
 				<h2> 2014 год </h2>
-				<p><strong>01 сентября 2014</strong></p>
+				<p><strong>13 октября 2014</strong></p>
+				<ul>
+					<li><a href="http://sro61.ru/docs/2014/reg_13.10.14.pdf" target="_blank">PDF-файл</a> со списком паспортов</li>
+				</ul>
+                <p><strong>01 сентября 2014</strong></p>
 				<ul>
 					<li><a href="http://sro61.ru/docs/2014/reg_01.09.14.pdf" target="_blank">PDF-файл</a> со списком паспортов</li>
 				</ul>
